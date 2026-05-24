bb59a5066226960179c62177f3d519439fe72016
Professor <forbeginners156@gmail.com>
2026-05-24 16:18:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const body =
+document.body;
+
+const toggleBtn =
+document.getElementById("theme-toggle");
+
+body.classList.add("dark-mode");
+
+toggleBtn.addEventListener("click",()=>{
+
+  body.classList.toggle("light-mode");
+
+  body.classList.toggle("dark-mode");
+
+  const icon =
+  toggleBtn.querySelector("i");
+
+  if(body.classList.contains("light-mode")){
+
+    icon.classList.remove("fa-moon");
+
+    icon.classList.add("fa-sun");
+
+  }
+
+  else{
+
+    icon.classList.remove("fa-sun");
+
+    icon.classList.add("fa-moon");
+
+  }
+
+});
